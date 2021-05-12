4b4af15b43ba60a8a147d234cfd23568d4b381cd
Glacias <klapkaivan@yahoo.fr>
2021-05-12 17:08:12 +0200

    Finished part2 ppo

--- ppo.py
+++ ppo.py
@@ -66,6 +66,10 @@ class PPO:
 		self.ts_per_batch = 5000 	# Number of timestep per batch
 		self.max_ts_per_ep = 1000 	# Maximum number of timestep per episode
 		self.std_dev = 0.5 			# Standart deviation for action selection
+		self.n_epoch = 5			# Number of epoch
+		self.epsilon = 0.2 			# Clipping value for the loss
+		self.lr_actor = 0.005		# Learning rate for the actor network
+		self.lr_critic = 0.005		# Learning rate for the critic network
 
 		## Setup environment
 		problem = "InvertedDoublePendulumPyBulletEnv-v0"
@@ -88,6 +92,10 @@ class PPO:
 		self.actor = Net_actor(self.num_states, self.num_actions)
 		self.critic = Net_critic(self.num_states, 1)
 
+		# Setup optimizer fo actor and critic
+		self.actor_optim = Adam(self.actor.parameters(), lr=self.lr_actor)
+		self.critic_optim = Adam(self.critic.parameters(), lr=self.lr_critic)
+
 		# Set the standart deviation for actions
 		self.std_vect = torch.full(size=(self.num_actions,), fill_value=self.std_dev)
 		
@@ -95,19 +103,56 @@ class PPO:
 		self.cov_mat = torch.diag(self.std_vect)
 
 		#### Test ####
-		t = torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5], dtype=torch.float)
-		a, p = self.policy(t)
-		bq, ba, blp, brtg, blen = self.generate_traj()
+		#t = torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5], dtype=torch.float)
+		#a, p = self.policy(t)
+		#bq, ba, blp, brtg, blen = self.generate_traj()
 
 
 	## Implentation of pseudo-code from PPO-clip
 	def run(self, total_ts=100000):
 		ts = 0	# Current number of timestep
-		while t_so_far < total_timesteps:
+		while ts < total_ts:
 			# Generate episodes (trajectories) with the actual policy
 			batch_state, batch_action, batch_log_prob, batch_rtg, batch_ep_len = self.generate_traj()
 			# Increment timestep by the number of timestep we used in the batch
 			ts += np.sum(batch_ep_len)
+
+			# Estimate the value function for each state and detach it
+			V_fct = self.estimate_V_fct(batch_state).detach()
+			# Compute the advantage of each state
+			Adv = batch_rtg - V_fct
+			# Normalize the advantage
+			Adv = (Adv - Adv.mean()) / (Adv.std() + 1e-10) # + 1e-10 is for not dividing by 0
+
+			# Main loop where the training will happen
+			for i in range(self.n_epoch):
+				# Estimate the value function for each state
+				V_fct = self.estimate_V_fct(batch_state)
+				# Compute the log probablities of actions from actual policy
+				actual_log_probs = self.compute_log_prob(batch_state, batch_action)
+
+				## Compute losses
+				# Compute the ratio of action probabilities
+				ratio_act_prob = torch.exp(actual_log_probs - batch_log_prob)
+				# Compute to two part of the loss
+				loss_p1 = actual_log_probs * Adv
+				loss_p2 = torch.clip(ratio_act_prob, 1 - self.epsilon, 1 + self.epsilon)
+				# Compute the loss of the actor
+				# since we want to do a gradient ascent instead of descent
+				# we do the inverse of the loss and take the mean
+				loss_actor = (-torch.min(loss_p1,loss_p2)).mean()
+				# Compute the critic loss
+				loss_critic = nn.MSELoss()(V_fct, batch_rtg)
+
+				## Backprop
+				# Gradient ascent for actor network
+				self.actor_optim.zero_grad()
+				loss_actor.backward(retain_graph=True)
+				self.actor_optim.step()
+				# Gradient descent for the critic network
+				self.critic_optim.zero_grad()
+				loss_critic.backward()
+				self.critic_optim.step()
 
 
 	# Generate a batch of episodes (trajectories) using the actual policy
@@ -145,10 +190,10 @@ class PPO:
 
 			# Store episode reward and length
 			batch_reward.append(ep_rewards)
-			batch_ep_len.append(ts+1)
+			batch_ep_len.append(ts_ep+1)
 
-		# Compute rewards to go for all the episodes in the batch 
-		batch_rtg = self.compute_batch_rtg(batch_reward) # Batch rewards to go 	 Shape(n_ep)
+		# Compute rewards to go for all the episodes in the batch
+		batch_rtg = self.compute_batch_rtg(batch_reward, batch_ep_len)
 
 		# Convert all the batch used for computation to tensors
 		batch_state = torch.tensor(batch_state, dtype=torch.float)
@@ -158,7 +203,7 @@ class PPO:
 
 		return batch_state, batch_action, batch_log_prob, batch_rtg, batch_ep_len
 
-
+	# Get the next action from current state by following the actual policy
 	def policy(self, state):
 		# Convert the state to a tensor if necessary
 		if isinstance(state, np.ndarray):
@@ -177,22 +222,42 @@ class PPO:
 
 
 	# Compute batch rewards to go based on the batch of rewards
-	def compute_batch_rtg(self, batch_reward):
+	def compute_batch_rtg(self, batch_reward, batch_ep_len):
 		# Store the batch of reward to go
-		batch_reward_to_go = []
+		batch_reward_to_go = np.zeros(np.sum(batch_ep_len))
+		# Starting index of the previous episode
+		i = 0
 		# For each episode
 		for ep_rewards in batch_reward:
-			# Store the value of the reward to go
-			reward_to_go = 0
+			# Size of the episode
+			size_ep = len(ep_rewards)
 			# Go trough all the reward starting for the end
-			for r in reversed(ep_rewards):
+			for j in reversed(range(size_ep)):
 				# Compute rewards to go
-				reward_to_go = r + reward_to_go * self.gamma
-			# Add the rewards to go to the batch
-			batch_reward_to_go.append(reward_to_go)
+				batch_reward_to_go[i+j] = ep_rewards[j] + (batch_reward_to_go[i+j+1] * self.gamma if j+1 < size_ep else 0)
+			# Increment i
+			i += size_ep
 		return batch_reward_to_go
+
+	# Estimate the value function V(s) by passing through the critic network
+	def estimate_V_fct(self, batch_state):
+		# Return estimation of V
+		return self.critic(batch_state).squeeze()
+
+	# Compute the log probability of actions 
+	def compute_log_prob(self, batch_state, batch_action):
+		# Pass the state trough the actor network
+		# The output should correspond to the mean of the action
+		mean = self.actor(batch_state)
+		# Create a multivariate normal distribution
+		norm_dist = MultivariateNormal(mean, self.cov_mat)
+		# Return log probability
+		return norm_dist.log_prob(batch_action)
+
+
 
 ### Main ###
 
 if __name__ == '__main__':
 	ppo = PPO()
+	ppo.run()
