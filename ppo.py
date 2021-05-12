cf3f1eca1ba0d629b6f9a5d47ade538afa51734c
Glacias <klapkaivan@yahoo.fr>
2021-05-12 02:57:32 +0200

    Improvements first part

--- ppo.py
+++ ppo.py
@@ -89,10 +89,15 @@ class PPO:
 		self.critic = Net_critic(self.num_states, 1)
 
 		# Set the standart deviation for actions
-  		self.std_vect = torch.full(size=(self.num_actions,), fill_value=std_dev)
-  
-  		# Create the covariance matrix
-  		self.cov_mat = torch.diag(self.std_vect)
+		self.std_vect = torch.full(size=(self.num_actions,), fill_value=self.std_dev)
+		
+		# Create the covariance matrix
+		self.cov_mat = torch.diag(self.std_vect)
+
+		#### Test ####
+		t = torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5], dtype=torch.float)
+		a, p = self.policy(t)
+		bq, ba, blp, brtg, blen = self.generate_traj()
 
 
 	## Implentation of pseudo-code from PPO-clip
@@ -111,36 +116,51 @@ class PPO:
 		batch_action = []		# Batch actions 			Shape(ts_per_batch, n_action)
 		batch_log_prob = []		# Batch log probabilities	Shape(ts_per_batch)
 		batch_reward = []		# Batch rewards 			Shape(n_ep, ts_per_ep)
-		batch_rtg = [] 			# Batch rewards to go 		Shape(n_ep)
 		batch_ep_len = [] 		# Batch length episode 		Shape(ts_per_batch)
 
 		ts = 0
 		while ts < self.ts_per_batch:
 			ep_rewards = []
-			state = self.env.rest()
+			state = self.env.reset()
 
 			for ts_ep in range(self.max_ts_per_ep):
 				# Store the state
 				batch_state.append(state)
 				# Choose an action to take based on the actual policy
 				action, log_prob = self.policy(state)
+				# Store actions and log_prob
+				batch_action.append(action)
+				batch_log_prob.append(log_prob)
 				# Do a step in the environment
 				state, reward, done, _ = self.env.step(action)
 				# Increment timestep
 				ts += 1 
-
-				# Store reward, action and log prob
+				# Store reward
 				ep_rewards.append(reward)
-				batch_action.append(action)
-				batch_log_prob.append(log_prob)
 
 				if done:
 					break
 
-			# Compute and stores rewards to go
+			# Store episode reward and length
+			batch_reward.append(ep_rewards)
+			batch_ep_len.append(ts+1)
+
+		# Compute rewards to go for all the episodes in the batch 
+		batch_rtg = self.compute_batch_rtg(batch_reward) # Batch rewards to go 	 Shape(n_ep)
+
+		# Convert all the batch used for computation to tensors
+		batch_state = torch.tensor(batch_state, dtype=torch.float)
+		batch_action = torch.tensor(batch_action, dtype=torch.float)
+		batch_log_prob = torch.tensor(batch_log_prob, dtype=torch.float)
+		batch_rtg = torch.tensor(batch_rtg, dtype=torch.float)
+
+		return batch_state, batch_action, batch_log_prob, batch_rtg, batch_ep_len
 
 
 	def policy(self, state):
+		# Convert the state to a tensor if necessary
+		if isinstance(state, np.ndarray):
+			state = torch.tensor(state, dtype=torch.float)
 		# Pass the state trough the actor network
 		# The output should correspond to the mean of the action
 		mean = self.actor(state)
@@ -149,9 +169,9 @@ class PPO:
 		# Sample from the distribution
 		action = norm_dist.sample()
 		# Compute log probability
-		log_prob = dist.log_prob(action)
+		log_prob = norm_dist.log_prob(action)
 		# Detach computational graph so that network does not compute gradient
-		return [action.detach()], log_prob.detach()
+		return action.detach().numpy(), log_prob.detach()
 
 
 	# Compute batch rewards to go based on the batch of rewards
@@ -162,13 +182,13 @@ class PPO:
 		for ep_rewards in batch_reward:
 			# Store the value of the reward to go
 			reward_to_go = 0
-    		# Go trough all the reward starting for the end
-    		for r in reversed(ep_rewards):
-    			# Compute rewards to go
-        		reward_to_go = r + reward_to_go * self.gamma
-        	# Add the rewards to go to the batch
-        	batch_reward_to_go.append(reward_to_go)
-    	return batch_reward_to_go
+			# Go trough all the reward starting for the end
+			for r in reversed(ep_rewards):
+				# Compute rewards to go
+				reward_to_go = r + reward_to_go * self.gamma
+			# Add the rewards to go to the batch
+			batch_reward_to_go.append(reward_to_go)
+		return batch_reward_to_go
 
 ### Main ###
 
